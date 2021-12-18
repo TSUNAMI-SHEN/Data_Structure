4d4faa36882890cc4b5f0648e844f7cf4c7b24aa
TSUNAMI-SHEN <74399586+TSUNAMI-SHEN@users.noreply.github.com>
2021-12-18 15:36:52 +0800

    Create 136-one-apperance.cpp

---
+++ LeetCode/136-one-apperance.cpp
@@ -0,0 +1,13 @@
+# 利用异或运算
+# 异或运算：两个值相同时返回0，两个值不同时返回1
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ret = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            ret = ret ^ nums[i];
+        }
+        return ret;
+    }
+};
