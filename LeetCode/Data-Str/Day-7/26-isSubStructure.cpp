14685359c3b52f12056e2fdaffffcecd0ef301fd
TSUNAMI-SHEN <74399586+TSUNAMI-SHEN@users.noreply.github.com>
2022-01-08 14:34:33 +0800

    Create 26-isSubStructure.cpp

---
+++ LeetCode/Data-Str/Day-7/26-isSubStructure.cpp
@@ -0,0 +1,24 @@
+//判断树B是不是树A的子结构
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSubStructure(TreeNode* A, TreeNode* B) {
+        return (A != nullptr && B != nullptr) && 
+        (recur(A,B) || isSubStructure(A->left, B) || isSubStructure(A->right, B));
+    }
+private:
+    bool recur(TreeNode* A, TreeNode* B)
+    {
+        if (B == nullptr) return true;
+        if (A == nullptr || A->val != B->val) return false;
+        return recur(A->left, B->left) && recur(A->right, B->right);
+    }
+};
