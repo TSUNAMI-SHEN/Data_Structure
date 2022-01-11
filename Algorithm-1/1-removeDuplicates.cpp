5cd4be3abef56fbf0ec206d1a5aeabc437220d86
TSUNAMI-SHEN <74399586+TSUNAMI-SHEN@users.noreply.github.com>
2022-01-11 20:35:56 +0800

    Create 1-removeDuplicates.cpp

---
+++ Algorithm-1/1-removeDuplicates.cpp
@@ -0,0 +1,20 @@
+//删除排序数组中的重复项
+//双指针，fast&slow双指针，当nums[fast] != nums[slow]时，交换两元素
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 0) {
+            return 0;
+        }
+        int fast = 1, slow = 1;
+        while (fast < n) {
+            if (nums[fast] != nums[fast - 1]) {
+                nums[slow] = nums[fast];
+                ++slow;
+            }
+            ++fast;
+        }
+        return slow;
+    }
+};
