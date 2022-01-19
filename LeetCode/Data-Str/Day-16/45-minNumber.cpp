bca5e7180fbf3a20dabce5fb2c450c2964eb287a
TSUNAMI-SHEN <74399586+TSUNAMI-SHEN@users.noreply.github.com>
2022-01-19 21:41:50 +0800

    Create 45-minNumber.cpp

---
+++ LeetCode/Data-Str/Day-16/45-minNumber.cpp
@@ -0,0 +1,34 @@
+//把数组排成最小的数
+//本质是排序问题，将x、y分别组成xy和yx两个数，若xy>yx，说明x需要排在y后面；若xy<yx，说明x需要排在y前面
+class Solution {
+public:
+    string minNumber(vector<int>& nums) {
+        vector<string> strs;
+        for(int i = 0; i < nums.size(); i++)
+        {
+            strs.push_back(to_string(nums[i]));
+        }
+        quickSort(strs, 0, strs.size() - 1);
+        string res;
+        for(string s : strs)
+        {
+            res.append(s);
+        }
+        return res;
+    }
+private:
+    void quickSort(vector<string>& strs, int l, int r)
+    {
+        if(l >= r) return;
+        int i = l, j = r;
+        while(i < j)
+        {
+            while(strs[j] + strs[l] >= strs[l] + strs[j] && i < j) j--;
+            while(strs[i] + strs[l] <= strs[l] + strs[i] && i < j) i++;
+            swap(strs[i], strs[j]);
+        }
+        swap(strs[i], strs[l]);
+        quickSort(strs, l, i-1);
+        quickSort(strs, i+1, r);
+    }
+};
