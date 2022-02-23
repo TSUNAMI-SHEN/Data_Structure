7de9cf3569f9daceeddbda41f0c454f119aea0dd
TSUNAMI-SHEN <74399586+TSUNAMI-SHEN@users.noreply.github.com>
2022-02-23 13:05:19 +0800

    Create 1_reverseString.py

---
+++ 刷题/字符串/1_reverseString.py
@@ -0,0 +1,12 @@
+# 设置前后双指针，不断交换并往中间移动
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        left = 0
+        right = len(s) - 1
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
