42146232dc5c15c358421c7231a90b62c8248d0b
TSUNAMI-SHEN <74399586+TSUNAMI-SHEN@users.noreply.github.com>
2022-02-23 12:36:24 +0800

    Create 9_fourSum.py

---
+++ 刷题/哈希表/9_fourSum.py
@@ -0,0 +1,28 @@
+# 双指针方法，在外面再嵌套一层循环
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        nums.sort()
+        n = len(nums)
+        res = []
+
+        for i in range(n):
+            if i > 0 and nums[i] == nums[i-1]: continue
+            for k in range(i+1, n):
+                if k > i + 1 and nums[k] == nums[k-1]: continue
+                left = k + 1
+                right = n - 1
+                
+                while left < right:
+                    total = nums[i] + nums[k] + nums[left] + nums[right]
+
+                    if total > target:
+                        right -= 1
+                    elif total < target:
+                        left += 1
+                    else:
+                        res.append([nums[i], nums[k], nums[left], nums[right]])
+                        while left != right and nums[left] == nums[left+1]: left += 1
+                        while left != right and nums[right] == nums[right-1]: right -= 1
+                        left += 1
+                        right -= 1
+        return res
