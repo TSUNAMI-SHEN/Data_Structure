89f1aa365d9154ecc2ee87ca635af1a49c170fbe
TSUNAMI-SHEN <74399586+TSUNAMI-SHEN@users.noreply.github.com>
2022-02-23 19:10:57 +0800

    Update 2_postorderTraversal.py

--- 刷题/二叉树/2_postorderTraversal.py
+++ 刷题/二叉树/2_postorderTraversal.py
@@ -17,3 +17,24 @@ class Solution:
 
         traversal(root)
         return result
+
+# 迭代法
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if root == None:
+            return []
+
+        result = []
+        stack = [root]
+
+        while stack:
+
+            node = stack.pop()
+            result.append(node.val)
+
+            if node.left:
+                stack.append(node.left)
+            if node.right:
+                stack.append(node.right)
+
+        return result[::-1]
