d9280be892f8f850130f561d0d0e072095398c9c
TSUNAMI-SHEN <74399586+TSUNAMI-SHEN@users.noreply.github.com>
2022-02-23 14:59:25 +0800

    Update 6_strStr.py

--- 刷题/字符串/6_strStr.py
+++ 刷题/字符串/6_strStr.py
@@ -37,3 +37,35 @@ class Solution:
                 k += 1
             next[i] = k
         return next
+    
+    
+# 第二种方法，前缀表不往前移一位
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        a = len(needle)
+        b = len(haystack)
+        if a == 0:
+            return 0
+        next = self.getNext(a, needle)
+        p = 0
+        for j in range(b):
+            while p > 0 and needle[p] != haystack[j]:    # 模式串与文本串出现不匹配，因为前缀表已经往前移一位了，所以只需关注冲突位置即可
+                p = next[p-1]
+            if needle[p] == haystack[j]:
+                p += 1
+            if p == a:
+                return j-a+1
+        return -1
+
+    def getNext(self, a, needle):
+        next = ['' for i in range(a)]
+        k = 0  # k是前缀末尾，也是i之前的最长相等前后缀的长度
+        next[0] = k
+        for i in range(1, len(needle)):
+            while (k>0 and needle[k]!=needle[i]):
+                k = next[k-1]
+            if needle[k] == needle[i]:
+                k += 1
+            next[i] = k
+        return next
+
