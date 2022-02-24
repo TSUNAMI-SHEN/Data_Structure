0378266c904a049beded2ecf4178e6071cd26222
TSUNAMI-SHEN <74399586+TSUNAMI-SHEN@users.noreply.github.com>
2022-02-24 12:01:21 +0800

    Create 19_binaryTreePaths.py

---
+++ 刷题/二叉树/19_binaryTreePaths.py
@@ -0,0 +1,51 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# 递归法
+class Solution:
+    def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
+        # 要做回溯，得先删除节点，然后再加入新的节点
+        path = ''
+        result = []
+        if not root: return result
+        self.traversal(root, path, result)
+        return result
+        
+    
+    def traversal(self, cur: TreeNode, path: str, result: List[str]) -> None:
+        path += str(cur.val)
+        # 若当前节点为leave，直接输出
+        if not cur.left and not cur.right:
+            result.append(path)
+        
+        if cur.left:
+            self.traversal(cur.left, path + '->', result)
+        if cur.right:
+            self.traversal(cur.right, path + '->', result)
+
+# 迭代法
+class Solution:
+    def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
+        stack = deque([root])
+        path_st = deque()
+        result = []
+        path_st.append(str(root.val))
+
+        while stack:
+            cur = stack.pop()
+            path = path_st.pop()
+
+            # 如果当前节点为叶子节点，添加路径到结果中
+            if not cur.left and not cur.right:
+                result.append(path)
+            if cur.right:
+                stack.append(cur.right)
+                path_st.append(path + '->' + str(cur.right.val))
+            if cur.left:
+                stack.append(cur.left)
+                path_st.append(path + '->' + str(cur.left.val))
+        return result
