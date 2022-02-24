011c8a55c405fb669036fa17ce475693d57a701e
TSUNAMI-SHEN <74399586+TSUNAMI-SHEN@users.noreply.github.com>
2022-02-24 10:40:29 +0800

    Update 15_minDepth.py

--- 刷题/二叉树/15_minDepth.py
+++ 刷题/二叉树/15_minDepth.py
@@ -22,3 +22,22 @@ class Solution:
             if cur.right:
                 que.append([cur.right, depth+1])
         return 0
+
+# 递归的方法
+# 如果左子树为空，右子树不为空，说明最小深度是 1 + 右子树的深度
+# 右子树为空，左子树不为空，最小深度是 1 + 左子树的深度
+# 如果左右子树都不为空，返回左右子树深度最小值 + 1 
+class Solution:
+    def minDepth(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        if not root.left and not root.right:    # 根节点的左右孩子都为空
+            return 1
+        
+        min_depth = 10 ** 6
+
+        if root.left:
+            min_depth = min(self.minDepth(root.left), min_depth)
+        if root.right:
+            min_depth = min(self.minDepth(root.right), min_depth)
+        return min_depth + 1
