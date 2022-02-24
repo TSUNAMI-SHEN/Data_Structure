c04d8391c4f9f6c2f9b3733e35cf4f59168bb40c
TSUNAMI-SHEN <74399586+TSUNAMI-SHEN@users.noreply.github.com>
2022-02-24 15:33:02 +0800

    Create 32_getMinimumDifference.py

---
+++ 刷题/二叉树/32_getMinimumDifference.py
@@ -0,0 +1,19 @@
+
+class Solution:
+    def getMinimumDifference(self, root: TreeNode) -> int:
+        stack = []
+        cur = root
+        pre = None  # 需要有一个pre节点记录一下当前节点的前一节点，从而计算差值
+        result = float("inf")
+
+        while cur or stack:
+            if cur:
+                stack.append(cur)
+                cur = cur.left  # 中序遍历，先将左节点都压入栈
+            else:
+                cur = stack.pop()   # 出栈，即输出中间节点
+                if pre:
+                    result = min(result, cur.val - pre.val)
+                pre = cur
+                cur = cur.right # 最后压入右节点
+        return result
