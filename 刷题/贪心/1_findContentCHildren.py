93e5aa150a46550b52f8cf7cff9a5c87000d1da2
TSUNAMI-SHEN <74399586+TSUNAMI-SHEN@users.noreply.github.com>
2022-02-26 10:42:15 +0800

    Create 1_findContentCHildren.py

---
+++ 刷题/贪心/1_findContentCHildren.py
@@ -0,0 +1,10 @@
+class Solution:
+    # 思路1：优先考虑胃饼干
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        g.sort()
+        s.sort()
+        res = 0
+        for i in range(len(s)):
+            if res <len(g) and s[i] >= g[res]:  #小饼干先喂饱小胃口
+                res += 1
+        return res
