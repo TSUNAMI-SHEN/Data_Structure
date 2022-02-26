91f6b97741a5b8f14b57c003eef572b4af56c921
TSUNAMI-SHEN <74399586+TSUNAMI-SHEN@users.noreply.github.com>
2022-02-26 20:32:20 +0800

    Update 13_maxProfit.py

--- 刷题/动态规划/13_maxProfit.py
+++ 刷题/动态规划/13_maxProfit.py
@@ -8,6 +8,6 @@ class Solution:
         dp[0][1] = 0
 
         for i in range(1, n):
-            dp[i][0] = max(dp[i-1][0], -prices[i])     # i当天持有股票，考虑两种情况，1.i-1天已经持有股票，i天不买入;2.第i天买入，持有现金即为-prices[i]
+            dp[i][0] = max(dp[i-1][0], -prices[i])     # i当天持有股票，考虑两种情况，1.i-1天已经持有股票，i天不买入;2.第i天买入，持有现金即为-prices[i](因为只能买卖一次，所以一定是-prices[i])
             dp[i][1] = max(dp[i-1][1], dp[i-1][0]+prices[i]) # i当天不持有股票，考虑两种情况，1.第i-1天就不持有股票，保持原状；2.第i天卖出
         return dp[n-1][1]
