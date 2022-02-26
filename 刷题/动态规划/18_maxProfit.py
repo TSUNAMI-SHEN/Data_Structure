83a8fae503ccde242fe126032e41a5b26f167750
TSUNAMI-SHEN <74399586+TSUNAMI-SHEN@users.noreply.github.com>
2022-02-26 22:27:47 +0800

    Update 18_maxProfit.py

--- 刷题/动态规划/18_maxProfit.py
+++ 刷题/动态规划/18_maxProfit.py
@@ -10,6 +10,6 @@ class Solution:
 
         for i in range(1, n):
             dp[i][0] = max(dp[i-1][0], dp[i-1][1]-prices[i])
-            dp[i][1] = max(dp[i-1][1], dp[i-1][0]+prices[i]-fee)
+            dp[i][1] = max(dp[i-1][1], dp[i-1][0]+prices[i]-fee)    # 加上一笔手续费
         return dp[-1][1]
         
