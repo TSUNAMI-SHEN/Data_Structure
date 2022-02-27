e9bc67f0dbc7980f0ddbbbf57bea433615fe23c9
TSUNAMI-SHEN <74399586+TSUNAMI-SHEN@users.noreply.github.com>
2022-02-27 16:53:35 +0800

    Update and rename 刷题/动态规划/20_findLengthOfLCIS.py to 刷题/动态规划/子序列问题/LQ674_findLengthOfLCIS.py

--- 刷题/动态规划/子序列问题/LQ674_findLengthOfLCIS.py
+++ 刷题/动态规划/子序列问题/LQ674_findLengthOfLCIS.py
@@ -6,7 +6,7 @@ class Solution:
         res = 1
 
         for i in range(1, len(nums)):
-            if nums[i] > nums[i-1]:
+            if nums[i] > nums[i-1]: # 如果当前的数大于前一个数，那么dp[i] = dp[i-1]+1，否则dp[i]变为1（这个放在初始化里）
                 dp[i] = dp[i-1]+1
             res = max(res, dp[i])
         return res
