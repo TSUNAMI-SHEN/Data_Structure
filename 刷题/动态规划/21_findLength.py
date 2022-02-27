9e4eba31192f1316a232f6518cc768a1b690daae
TSUNAMI-SHEN <74399586+TSUNAMI-SHEN@users.noreply.github.com>
2022-02-27 09:28:31 +0800

    Update 21_findLength.py

--- 刷题/动态规划/21_findLength.py
+++ 刷题/动态规划/21_findLength.py
@@ -9,3 +9,18 @@ class Solution:
                     dp[i][j] = dp[i-1][j-1] + 1
                 result = max(result, dp[i][j])
         return result
+
+# 滚动数组的情况
+class Solution:
+    def findLength(self, nums1: List[int], nums2: List[int]) -> int:
+        dp = [0] * (len(nums2)+1)
+        result = 0
+        
+        for i in range(1, len(nums1)+1):
+            for j in range(len(nums2), 0, -1):
+                if nums1[i-1] == nums2[j-1]:
+                    dp[j] = dp[j-1] + 1
+                else:
+                    dp[j] = 0   # 如果不相等，则dp[j]变为0
+                result = max(result, dp[j])
+        return result
