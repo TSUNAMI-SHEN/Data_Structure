41df6dd9e9169d22e6a150f071f30fdd089d0031
TSUNAMI-SHEN <74399586+TSUNAMI-SHEN@users.noreply.github.com>
2022-02-28 10:44:35 +0800

    Update and rename 刷题/二叉树/12_connect.py to 刷题/二叉树/层序遍历/LQ116_connect.py

--- 刷题/二叉树/层序遍历/LQ116_connect.py
+++ 刷题/二叉树/层序遍历/LQ116_connect.py
@@ -9,24 +9,6 @@ class Node:
 """
 # 层次遍历
 # 在单层遍历的时候记录一下本层的头部节点，然后在遍历的时候让前一个节点指向本节点就可以了
-class Solution:
-    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
-        if not root:
-            return None
-        queue = [root]
-
-        while queue:
-            n = len(queue)
-            for i in range(n):
-                node = queue.pop(0)
-                if node.left:
-                    queue.append(node.left)
-                if node.right:
-                    queue.append(node.right)
-                if i == n - 1:
-                    break
-                node.next = queue[0]
-        return root
 # 链表法
 class Solution:
     def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
@@ -39,3 +21,4 @@ class Solution:
                 cur = cur.next  # cur同层移动到下一结点
             first = first.left  # 从本层拓展到下一层
         return root
+   
