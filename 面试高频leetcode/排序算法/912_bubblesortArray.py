7e58c9bf7c3cc5146ac595997222baa96ef49d2b
tsunami-shen <74399586+TSUNAMI-SHEN@users.noreply.github.com>
2022-04-03 14:21:28 +0800

    Update 912_bubblesortArray.py

--- 面试高频leetcode/排序算法/912_bubblesortArray.py
+++ 面试高频leetcode/排序算法/912_bubblesortArray.py
@@ -1,9 +1,9 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
         # 冒泡排序：每次找出一个最大的元素，因此需要遍历n-1次
-        flag = False
 
         for i in range(len(nums)-1):
+            flag = False
             for j in range(len(nums)-i-1):
                 if nums[j] > nums[j+1]:
                     nums[j+1], nums[j] = nums[j], nums[j+1]
